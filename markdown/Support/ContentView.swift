a8b323e5b4c657d7187b5d810a80562a11319dc1
Annurdien Rasyid <annurdien.dev@gmail.com>
2024-10-06 11:50:17 +0700

    initiate project

---
+++ markdown/Support/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  markdown
+//
+//  Created by Annurdien Rasyid on 06/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ markdown/Support/Main.swift
@@ -0,0 +1,17 @@
+//
+//  markdownApp.swift
+//  markdown
+//
+//  Created by Annurdien Rasyid on 06/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Markdown: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
